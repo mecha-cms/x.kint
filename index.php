3ba04bd55344eb6d34117ad6d7bb8f918cb1b354
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-10-09 23:18:34 +0700

    Update

--- index.php
+++ index.php
@@ -5,14 +5,12 @@ require __DIR__ . D . 'kint.phar';
 Kint::$app_root_dirs = [PATH => '.'];
 Kint::$display_called_from = false;
 
-function kint($lot) {
-    $fn = new ReflectionMethod('Kint::dump');
-    $fn->invokeArgs((object) [], [$lot]);
+function kint(...$var) {
+    Kint::dump(...$var);
 }
 
 if (defined('TEST') && TEST && ($kint = $_REQUEST['kint'] ?? null)) {
     if (is_string($kint)) {
-        [$id, $stack] = explode(':', $kint . ':');
         // `?kint=asdf:20`
         if (false !== strpos($kint, ':')) {
             [$id, $stack] = explode(':', $kint, 2);
@@ -20,14 +18,21 @@ if (defined('TEST') && TEST && ($kint = $_REQUEST['kint'] ?? null)) {
         // `?kint=asdf`
         } else {
             $id = $kint;
-            $stack = 10000; // Make hook stack value as large as possible so that it will be executed last!
+            $stack = 10000; // Make the stack value as large as possible so that it will be executed as late as possible
         }
-        Hook::set($id, function (...$lot) {
-            kint(...$lot); // Why this is not working? :(
+        Hook::set($id, function (...$v) {
+            kint(...$v);
         }, $stack);
     // `?kint=true`
     } else if (true === $kint) {
-        kint($GLOBALS);
+        Kint::dump(
+            $_COOKIE,
+            $_ENV,
+            $_GET,
+            $_POST,
+            $_REQUEST,
+            $_SERVER,
+        );
     }
     exit;
 }
