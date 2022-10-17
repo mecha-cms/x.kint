30d5affd3ed0a19e9391138ecedf899950dc2ce8
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-10-17 22:42:34 +0700

    Update

--- index.php
+++ index.php
@@ -1,38 +1,16 @@
 <?php
 
+define('KINT_SKIP_HELPERS', true);
 require __DIR__ . D . 'kint.phar';
 
+// <https://kint-php.github.io/kint/settings>
 Kint::$app_root_dirs = [PATH => '.'];
 Kint::$display_called_from = false;
+Kint\Renderer\PlainRenderer::$theme = 'plain.css';
+Kint\Renderer\RichRenderer::$theme = 'original.css';
 
-function kint(...$var) {
-    Kint::dump(...$var);
+function kint(...$lot) {
+    return Kint::dump(...$lot);
 }
 
-if (defined('TEST') && TEST && ($kint = $_REQUEST['kint'] ?? null)) {
-    if (is_string($kint)) {
-        // `?kint=asdf:20`
-        if (false !== strpos($kint, ':')) {
-            [$id, $stack] = explode(':', $kint, 2);
-            $stack = is_numeric($stack) ? (float) $stack : 10000;
-        // `?kint=asdf`
-        } else {
-            $id = $kint;
-            $stack = 10000; // Make the stack value as large as possible so that it will be executed as late as possible
-        }
-        Hook::set($id, function (...$v) {
-            kint(...$v);
-        }, $stack);
-    // `?kint=true`
-    } else if (true === $kint) {
-        Kint::dump(
-            $_COOKIE,
-            $_ENV,
-            $_GET,
-            $_POST,
-            $_REQUEST,
-            $_SERVER,
-        );
-    }
-    exit;
-}
+Kint::$aliases[] = 'kint';
