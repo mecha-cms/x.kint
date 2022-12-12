19023c5f386b846d04e142c5c6cf04436031bf74
Taufik Nurrohman <hi@taufik-nurrohman.com>
2022-12-12 22:36:56 +0700

    Increase Depth Limit

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ Kint Extension for [Mecha](https://github.com/mecha-cms/mecha)
 
 ![Code Size](https://img.shields.io/github/languages/code-size/mecha-cms/x.kint?color=%23444&style=for-the-badge)
 
-![Kint](index.png?v=2022-12-03)
+![Kint](index.png?v=2022-12-12)
--- index.php
+++ index.php
@@ -4,7 +4,9 @@ define('KINT_SKIP_HELPERS', true);
 require __DIR__ . D . 'kint.phar';
 
 // <https://kint-php.github.io/kint/settings>
+Kint::$aliases[] = 'kint';
 Kint::$app_root_dirs = [PATH => '.'];
+Kint::$depth_limit = 50;
 Kint::$display_called_from = false;
 Kint\Renderer\PlainRenderer::$theme = 'plain.css';
 Kint\Renderer\RichRenderer::$theme = 'original.css';
@@ -12,5 +14,3 @@ Kint\Renderer\RichRenderer::$theme = 'original.css';
 function kint(...$lot) {
     return Kint::dump(...$lot);
 }
-
-Kint::$aliases[] = 'kint';
